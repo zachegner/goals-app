7048d5ad9333c9dc3a36fef981ceb21aca66bb1b
kongman551 <eggy551@gmail.com>
2022-03-23 15:50:54 -0400

    adds production deployment build

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
-const connectDB = require ('./config/db')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 const { errorHandler } = require('./middleware/errorMiddleware')
 
@@ -10,12 +11,26 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extened: false}))
+app.use(express.urlencoded({ extened: false }))
 
 const goalRoutes = require('./routes/goalRoutes')
 const userRoutes = require('./routes/userRoutes')
+const req = require('express/lib/request')
 app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
